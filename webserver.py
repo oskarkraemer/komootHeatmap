fa51dd1c9742bf3c7163fb9fb86dee57878eb0df
oskarkraemer <oskarkraemer@gmail.com>
2023-04-10 15:02:45 +0200

    merged functions + changed to cookies

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, Response, session
+from flask import Flask, render_template, request, redirect, url_for, Response, make_response
 import get_data
 import time
 import json
@@ -10,24 +10,21 @@ import json
 
 app = Flask(__name__)
 
-@app.before_request
-def make_session_permanent():
-    session.permanent = True
 
 @app.route('/')
 def home():
-    if 'userid' not in session:
+    if not is_logged_in():
         return redirect(url_for('login'))
 
-    if not get_data.check_auth(session['userid']):
-        return redirect(url_for('login'))
-    
     return render_template('index.html')
 
 
 @app.route('/tours_data')
 def gpx():
-    gpx_data = get_data.get_all_tours_gpx(session['userid'])
+    if not is_logged_in():
+        return Response(status=401)
+
+    gpx_data = get_data.get_all_tours_gpx(request.cookies.get('email'), request.cookies.get('password'), request.cookies.get('userid'))
 
     return Response(json.dumps(gpx_data), mimetype='application/json')
 
@@ -39,22 +36,18 @@ def login():
         password = request.form['password']
         userid = request.form['userid']
 
-        #either email or password is wrong
-        if not get_data.auth(email, password):
+        #Check if credentials are correct
+        if not get_data.auth(email, password, userid):
             return redirect(url_for('login'))
         
-        #Userid is most likely wrong
-        tours = get_data.get_tours_data(userid)
-        if tours == False:
-            return redirect(url_for('login'))
-        
-        #Everything is fine
-        session['email'] = email
-        session['password'] = password
-        session['userid'] = userid
+        #Everything is fine / set cookies
+        resp = make_response(redirect(url_for('home')))
 
-        
-        return redirect(url_for('home'))
+        resp.set_cookie('email', email)
+        resp.set_cookie('password', password)
+        resp.set_cookie('userid', userid)
+
+        return resp
     else:
         return render_template('login.html')
 
@@ -71,8 +64,21 @@ def pin_icon_end():
 def pin_shadow():
     return Response(status=418)
 
+
+"""
+Check if auth cookies are valid
+"""
+def is_logged_in():
+    cookies = request.cookies
+    if 'userid' not in cookies or 'email' not in cookies or 'password' not in cookies:
+        return False
+    
+    if not get_data.auth(cookies.get('email'), cookies.get('password'), cookies.get('userid')):
+        return False
+    
+    return True
+
 if __name__ == '__main__':
-    app.secret_key = 'cheeser9387fdj9439ds'
-    app.config['SESSION_TYPE'] = 'filesystem'
+    app.secret_key = 'pU4VmigPwFcA1337'
 
     app.run(debug=False)
